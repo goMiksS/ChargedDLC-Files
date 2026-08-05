9e0326eecc2b237ad7482c9e74ca9ce8942648bb
goMiksS <miksikdim@gmail.com>
2026-08-05 22:10:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+# ==================== КОНФИГ ====================
+BOT_TOKEN = "8836466924:AAG9JHWBmNN9voqfWY-6aRK3PdKl4eJ46VU"
+ADMIN_ID = 5115505064
+
+# Настройки хранения
+MEDIA_DIR = "media"
+DB_PATH = "database.db"
+MAX_MESSAGES_PER_USER = 5000
+AUTO_CLEAN_DAYS = 30
+KEEP_OWN_MESSAGES = True
+
+# Префикс пользовательских команд
+CMD_PREFIX = "."
+
+# Версия
+VERSION = "5.0.0-ultra"
+BOT_NAME = "SaveMod++ by Grok"
