5f7836f35fea93ae997cb9bcd7797b5dc93fc899
goMiksS <miksikdim@gmail.com>
2026-08-05 20:36:43 +0300

    Add API_ID, API_HASH, and PHONE_NUMBER to config
    Updated configuration to include API_ID, API_HASH, and PHONE_NUMBER from environment variables.

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8836466924:AAG9JHWBmNN9voqfWY-6aRK3PdKl4eJ46VU")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+PHONE_NUMBER = os.getenv("+79036476677")
 ADMIN_ID = int(os.getenv("5115505064", 0))
 DB_NAME = "messages.db"
-
-# Настройки логирования
-LOG_CHAT_ID = ADMIN_ID  # Куда отправлять логи
