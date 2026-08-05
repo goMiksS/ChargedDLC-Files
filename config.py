c9ea19cfb39dafa709c774610449ebb89c59c0ef
goMiksS <miksikdim@gmail.com>
2026-08-05 20:18:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+BOT_TOKEN = os.getenv("8836466924:AAG9JHWBmNN9voqfWY-6aRK3PdKl4eJ46VU")
+ADMIN_ID = int(os.getenv("5115505064", 0))
 DB_NAME = "messages.db"
 
 # Настройки логирования
