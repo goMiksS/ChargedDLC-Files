498dc7d68bdadd3412c21906beb2fb176d39adcb
goMiksS <miksikdim@gmail.com>
2026-08-06 04:55:13 +0300

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,19 @@
+from .database import (
+    init_db,
+    register_user,
+    save_message,
+    get_saved_message,
+    mark_deleted,
+    save_edit,
+    get_user_settings,
+)
 
+__all__ = [
+    "init_db",
+    "register_user",
+    "save_message",
+    "get_saved_message",
+    "mark_deleted",
+    "save_edit",
+    "get_user_settings",
+]
